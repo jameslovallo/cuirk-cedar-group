cdfd18b761727b9c9df70a09221153ea0b4d76c2
James Lovallo <james.lovallo@thrivent.com>
2024-09-09 15:50:32 -0400

    .

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,42 +1,79 @@
 import { html, loop, scss } from 'cuirk'
+import footerImages from '../data/footer.js'
 import meta from '../data/meta.js'
 import nav from '../data/nav.js'
+import { card, cardBody, grid } from './index.js'
 
 const date = new Date().getFullYear()
 
 export const footer = () => html`
 	<footer>
-		<div>
-			${loop(nav, ({ href, title }) => html`<a href="${href}">${title}</a>`)}
+		<div class="top">
+			${grid({
+				children: loop(footerImages, ({ img, subtitle }) =>
+					card({
+						children: [html`<img src="${img}" />`, cardBody({ subtitle })],
+					})
+				),
+			})}
 		</div>
-		<small>© ${date} ${meta.title}. All Rights Reserved.</small>
+		<div class="bottom">
+			<div>
+				${loop(nav, ({ href, title }) => html`<a href="${href}">${title}</a>`)}
+			</div>
+			<small>© ${date} ${meta.title}. All Rights Reserved.</small>
+		</div>
 	</footer>
 `
 
 footer.style = scss`
 	footer {
-		background: var(--footer, black);
-		color: var(--footer-color, white);
-		display: grid;
-		gap: 1.5rem;
-		justify-content: center;
-		padding: 2rem 1rem;
 		position: sticky;
 		text-align: center;
 		top: 100vh;
+
+		.top {
+			background: #ddd;
+			padding: 1rem;
+
+			.grid {
+				margin: 0 auto;
+				max-width: 70ch;
+
+				.card {
+					img {
+						aspect-ratio: 4/3;
+						margin: 0 auto;
+						object-fit: contain;
+						object-position: center;
+						width: 90%;
+					}
+				}
+			}
+		}
 		
-		> div {
-			display: flex;
+		.bottom {
+			background: var(--c-primary);
+			color: var(--footer-color, white);
+			display: grid;
 			flex-wrap: wrap;
-			gap: 1rem;
+			gap: 1.5rem;
 			justify-content: center;
+			padding: 2rem 1rem;
 
-			> a {
-				color: inherit;
-				text-decoration: none;
+			> div {
+				display: flex;
+				flex-wrap: wrap;
+				gap: 1rem;
+				justify-content: center;
 
-				&:hover {
-					text-decoration: underline;
+				a {
+					color: inherit;
+					text-decoration: none;
+
+					&:hover {
+						text-decoration: underline;
+					}
 				}
 			}
 		}
---
+++ src/data/footer.js
@@ -0,0 +1,10 @@
+export default [
+	{
+		img: '/src/images/nmsdc.jpeg',
+		subtitle: 'NMSDC Certified',
+	},
+	{
+		img: '/src/images/sam.svg',
+		subtitle: 'Approved US Government Vendor',
+	},
+]
